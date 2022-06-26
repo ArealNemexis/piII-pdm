6f7fad802de668fd8928eaecd05713cdbb5f61aa
WillianMSantos <william.marreiros@gmail.com>
2022-06-25 23:22:48 -0300

    Update home_page.dart
    Retirando código sem necessidade da home.

--- lib/src/features/home/presentation/view/home_page.dart
+++ lib/src/features/home/presentation/view/home_page.dart
@@ -13,37 +13,4 @@ class HomePage extends StatelessWidget {
     );
   }
 
-
-    Widget get _map => Column(
-    children: [
-      SizedBox(
-        height: 30,
-        width: 175,
-        child: Text('map'.i18n(),
-            style: const TextStyle(
-            fontFamily: 'Nunito',
-            fontSize: 16,
-            fontWeight: FontWeight.bold,
-            color: AppColors.text, 
-            ),
-            textAlign: TextAlign.center,
-          ),
-        ),
-      GestureDetector(
-        onTap: () {
-          Modular.to.pushNamed('map');
-        },
-        child: SizedBox(
-          height: 110,
-          width: 175,
-          child: Image.asset('lib/assets/images/mapa.png',
-            width: 104,
-            height: 104,
-            fit: BoxFit.scaleDown,
-            ),
-          ),
-        ),
-    ],
-  );
-
 }
